aa06510974ba234e342841f21a4bc1e8b661e661
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 13:23:29 +0700

    add generator

--- blocks/blocks_ir-receiver.js
+++ blocks/blocks_ir-receiver.js
@@ -1,15 +1,22 @@
-Blockly.Blocks['ky_022_ir_receiver_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-022 IR RECEIVER");
-    this.appendValueInput("KY_022_IR_RECEIVER_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+module.exports = function(Blockly) {
+  Blockly.Blocks["ky_022_ir_receiver_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-022 IR RECEIVER");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "KY_022_IR_RECEIVER_PIN");
+      this.appendDummyInput()
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_022_IR_RECEIVER_VALUE"),
+          "KY_022_IR_RECEIVER_VALUE"
+        );
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
 };
--- blocks/generators_ir-receiver.js
+++ blocks/generators_ir-receiver.js
@@ -1,6 +1,31 @@
-Blockly.JavaScript['ky_022_ir_receiver_block'] = function(block) {
-  var value_ky_022_ir_receiver_pin = Blockly.JavaScript.valueToCode(block, 'KY_022_IR_RECEIVER_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_022_ir_receiver_block"] = function(block) {
+    var text_ky_022_ir_receiver_pin = block.getFieldValue(
+      "KY_022_IR_RECEIVER_PIN"
+    );
+    var variable_ky_022_ir_receiver_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_022_IR_RECEIVER_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#EXTINC
+	#include <IRremote.h>
+
+	int KY_022_IR_RECEIVER_PIN = ${text_ky_022_ir_receiver_pin}; // define input pin on Arduino 
+	IRrecv irrecv(KY_022_IR_RECEIVER_PIN); 
+	decode_results results; // decode_results class is defined in IRremote.h
+	#END
+
+	#SETUP
+	irrecv.enableIRIn(); // Start the receiver 
+	#END
+
+	if (irrecv.decode(&results)) {
+			// Serial.println(results.value, HEX); 
+			${variable_ky_022_ir_receiver_value} = results.value;
+			irrecv.resume(); // Receive the next value 
+		}
+		delay (100); // small delay to prevent reading errors
+    `;
+    return code;
+  };
